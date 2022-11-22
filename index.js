a51778cfd6ff4c1bdb7d2835e9a7769d39862403
Caleb Martinez <calebmartinez0929@gmail.com>
2022-11-22 15:03:25 -0600

    api lista

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express()
+const PORT = 3000;
+const names = require("./names");
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.json(names)
+})
+
+//Get one anime
+app.get("/api/:id", (req, res) => {
+    const animeName = names.find(c => c.id === parseInt(req.params.id))
+
+    if(!animeName) { res.status(404).send("Anime name not found")}
+
+    res.send(animeName)
+})
+
+app.listen(PORT)
---
+++ names.js
@@ -0,0 +1,181 @@
+module.exports = [
+    /*
+        1: Naruto
+        2: Shingeki
+        3: Dragon Ball
+        4: Chainsaw Man
+        5: One Piece
+        6: Jujutsu Kaisen
+        7: Kimetsu no Yaiba
+    */
+    {
+        id: 1,
+        animeName: "Naruto",
+        characters: [
+            "Naruto",
+            "Sasuke",
+            "Sakura",
+            "Kakashi",
+            "Sai",
+            "Gai",
+            "RockLee",
+            "Neji",
+            "Hinata",
+            "Gaara",
+            "Jiraiya",
+            "Tsunade",
+            "Orochimaru",
+            "Kanuto",
+            "Madara",
+            "Hashirama",
+            "Obito",
+            "Tobirama",
+            "Rin"
+        ]
+    },
+    {
+        id: 2,
+        animeName: "Shingeki no Kyojin",
+        characters: [
+            "Eren",
+            "Mikasa",
+            "Armin",
+            "Levi",
+            "Hange",
+            "Erwin",
+            "Jean",
+            "Connie",
+            "Sasha",
+            "Falco",
+            "Gabi",
+            "Reiner",
+            "Annie",
+            "Pieck"
+        ]
+    },
+    {
+        id: 3,
+        animeName: "Dragon Ball",
+        characters: [
+            "Goku",
+            "Vegeta",
+            "Gohan",
+            "Krilin",
+            "Roshi",
+            "Bulma",
+            "Piccolo",
+            "Milk",
+            "Goten",
+            "Trunks",
+            "Gotenks",
+            "Pan",
+            "17",
+            "18",
+            "Freezer",
+            "Broly",
+            "Satan",
+            "MajinBoo",
+            "Cell",
+            "Nappa",
+            "Bills",
+            "Wiss",
+            "ZenoSama"
+        ]
+    },
+    {
+        id: 4,
+        animeName: "Chainsaw Man",
+        characters: [
+            "Denji",
+            "Makima",
+            "Power",
+            "Aki",
+            "Pochita",
+            "Himeno",
+            "Kobeni",
+            "Demonio pistola",
+            "Kishibe",
+            "Demonio de control",
+            "Demonio bomba",
+            "Demonio katana",
+            "Demonio murcielago"
+        ]
+    }, 
+    {
+        id: 5,
+        animeName: "One Piece",
+        characters: [
+            "Luffy",
+            "Zoro",
+            "Sanji",
+            "Nami",
+            "Robin",
+            "Franky",
+            "Brook",
+            "Ace",
+            "Garp",
+            "Usopp",
+            "Chopper",
+            "Sabo",
+            "Gold D Roger",
+            "Shanks",
+            "Cocodrile",
+            "Doquixote Doflamingo",
+            "Barba blanca",
+            "Barba negra",
+            "Merry",
+            "Bochan",
+            "Kobi",
+
+        ]
+    },
+    {
+        id: 6,
+        animeName: "Jujutsu kaisen",
+        characters: [
+            "Yuji Itadori",
+            "Nobara",
+            "Megumi",
+            "Gojo",
+            "Yuta",
+            "Nanami",
+            "Sukuna",
+            "Inumaki",
+            "Maki",
+            "Toji",
+            "Geto",
+            "Miwa",
+            "Rika",
+            "kusakabe",
+
+        ]
+    },
+    {
+        id: 7,
+        animeName: "Kimetsu no Yaiba",
+        characters: [
+            "Tanjiro",
+            "Nezuko",
+            "Zenitsu",
+            "Inosuke",
+            "Rengoku",
+            "Muzan",
+            "Tomioka",
+            "Uzui",
+            "Kanao",
+            "Akaza",
+            "El patron",
+            "Sanemi",
+            "Tokito",
+            "Urokodaki",
+            "Enmu",
+            "Tamayo",
+            "Mitsuri",
+            "Shinobu",
+            "Yushiro",
+            "Daki",
+            "Gyutaro",
+            "Rui"
+        ]
+    }
+]
